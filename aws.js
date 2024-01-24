4a12e912135678b0c9e995f4efdab9d15f26e899
Sanketghule09 <155476382+Sanketghule09@users.noreply.github.com>
2024-01-24 13:05:26 +0530

    Add files via upload

---
+++ aws.js
@@ -0,0 +1,45 @@
+const leftbtn document.querySelector(".1-btn"); 
+ const rightbtn = document.querySelector(".r-btn");
+ Zoomed to fill
+@M
+nCyberLink
+rightbtn.addEventListener('click', function(event) ;
+console.log("done");
+const conent- document.querySelector('.product-slide');
+conent.scrollLeft += 1100;
+B event.preventDefault();
+});
+leftbtn.addEventListener('click', function(event) {
+console.log("done");
+const conent - document.querySelector('.product-slide');
+conent.scrollLeft 1100;
+event.preventDefault();
+}) I
+const leftbtn document.querySelector(".1-btn");
+const rightbtn = document.querySelector(".r-btn");
+rightbtn.addEventListener('click', function(event) {
+console.log('done');
+const conent= document.querySelector(.product-slide');
+conent.scrollLeft += 1100;
+event.preventDefault();
+});
+leftbtn.addEventListener('click', function(event) {
+console.log("done");
+const conent= document.querySelector('.product-slide');
+conent.scrollLeft 1100;
+event.preventDefault();
+const leftbtn document.querySelector(".1-btn");
+const rightbtn = document.querySelector(".r-btn");
+rightbtn.addEventListener('click', function(event) {
+console.log('done');
+const conent= document.querySelector(.product-slide');
+conent.scrollLeft += 1100;
+event.preventDefault();
+});
+leftbtn.addEventListener('click', function(event) {
+console.log("done");
+const conent= document.querySelector('.product-slide');
+conent.scrollLeft 1100;
+event.preventDefault();
+
+
